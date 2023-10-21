9c617cd631cea728e8c65a69319afefef0af7d2c
JuanGonzalezJara <juan.m.gonzalez.j@gmail.com>
2023-10-20 22:00:51 -0300

    Feat: add devise fields, add scaffold posts and comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Validaciones
+  validates :name, presence: true, length: { maximum: 100 }
+  enum role: [:client, :admin]
 end
